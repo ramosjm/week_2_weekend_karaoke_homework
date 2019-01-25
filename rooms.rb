56185b95ead95fa359dfb13a86bbf6a0cf1246be
ramosjm <jjramos_2004@hotmail.com>
2019-01-25 14:18:04 +0000

    room test can create room pass

---
+++ rooms.rb
@@ -0,0 +1,11 @@
+class Room
+  attr_accessor :room_name
+
+  def initialize(room_name,guest)
+    @room_name = room_name
+    @guest = guest
+  end
+
+
+
+end
--- specs/rooms_spec.rb
+++ specs/rooms_spec.rb
@@ -1,3 +1,19 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../rooms.rb")
+require_relative("../songs.rb")
+require_relative("../guests.rb")
+
+class RoomTest < MiniTest::Test
+
+  def setup()
+    @guest = Guest.new("Nancy Drew")
+    @room = Room.new("AC/DC",@guest)
+  end
+
+  def test_can_create_room()
+    assert_equal(Room,@room.class)
+  end
+
+
+end
