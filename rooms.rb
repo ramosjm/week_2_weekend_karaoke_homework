6b9a2c8f1bf52c0576e414ebe0057506c5e47a8d
ramosjm <jjramos_2004@hotmail.com>
2019-01-25 14:39:38 +0000

    add guest test redone

--- rooms.rb
+++ rooms.rb
@@ -1,14 +1,18 @@
 class Room
-  attr_accessor :room_name, :guest
+  attr_accessor :room_name
 
-  def initialize(room_name,guest)
+  def initialize(room_name)
     @room_name = room_name
-    @guest = guest
+    @guest = []
     @playlist = []
   end
+  def add_guest_to_room(guest)
+    return @guest << guest.guest_name
+  end
+
 
   def add_song_to_playlist(song)
-    return @playlist <<song
+    return @playlist << song
   end
 
 
--- specs/rooms_spec.rb
+++ specs/rooms_spec.rb
@@ -8,7 +8,7 @@ class RoomTest < MiniTest::Test
 
   def setup()
     @guest = Guest.new("Nancy Drew")
-    @room = Room.new("AC/DC",@guest)
+    @room = Room.new("AC/DC")
     @song = Song.new("Thunderstruck")
   end
 
@@ -20,9 +20,13 @@ class RoomTest < MiniTest::Test
     assert_equal("AC/DC",@room.room_name)
   end
 
-  def test_has_guest()
-    assert_equal("Nancy Drew",@guest.guest_name)
+  def test_add_guest_to_room()
+    assert_equal(["Nancy Drew"],@room.add_guest_to_room(@guest))
   end
+
+  # def test_has_guest()
+  #   assert_equal(["Nancy Drew"],@guest.guest_name)
+  # end
 
   def test_add_song_to_playlist()
     assert_equal("Thunderstruck",@song.song_title)
